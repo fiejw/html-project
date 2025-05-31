5067dfc0d7e1a8bdfe9055873ab3c762c91d090a
guswnd4972 <guswnd4972@gmail.com>
2025-05-31 20:12:05 +0900

    문제 변경, ox가 아닌 문제에 따라 답 변경, 문제에 따라서 사진 변경

--- ox.js
+++ ox.js
@@ -2,30 +2,57 @@ const correctSound = new Audio("https://www.higherlowerkorea.com/sounds/yes.mp3"
 const wrongSound = new Audio("https://www.higherlowerkorea.com/sounds/no.mp3");
 
 const questions = [
-            { text: "대한민국의 수도는 서울인가?", correct: true },
-            { text: "파이썬은 고급언어인가?", correct: true },
-            { text: "거짓", correct: false }
-        ];
+    { text: "대한민국의 수도는 어디인가?", options: ["서울", "부산"], correct: "서울",
+        background:['https://images.pexels.com/photos/237211/pexels-photo-237211.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            'https://images.pexels.com/photos/19197793/pexels-photo-19197793.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        ]
+    },
+    { text: "고대 로마에서 사용된 고문 도구는?", options: ["장미가시", "염소"], correct: "염소",
+        background:['https://images.pexels.com/photos/15239/flower-roses-red-roses-bloom.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            'https://images.pexels.com/photos/144240/goat-lamb-little-grass-144240.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2']
+    },
+    { text: "스위스에서 밤 10시 이후에 불법인 것은? ", options: ["변기 물 내리기", "색소폰 불기"], correct: "변기 물 내리기",
+        background:['https://images.pexels.com/photos/7545637/pexels-photo-7545637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            'https://images.pexels.com/photos/1049690/pexels-photo-1049690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        ]
+     }
+];
 
-        let currentQuestionIndex = 0;
+let QuestionNumber = 0;
 
-        function loadQuestion() {
-            document.getElementById("question").innerText = questions[currentQuestionIndex].text;
-        }
+function loadQuestion() {
+  document.getElementById("question").innerText = questions[QuestionNumber].text;
+  document.querySelector(".co").innerText = questions[QuestionNumber].options[0];
+  document.querySelector(".cf").innerText = questions[QuestionNumber].options[1];
+  
+  // img1 배경 변경
+  document.querySelector(".img1").style.background = `
+    linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) 50% 50% / auto 100% no-repeat, 
+    url(${questions[QuestionNumber].background[0]})
+  `;
+  document.querySelector(".img1").style.backgroundPosition = "50% 50%";
 
-        function nextQuestion(userAnswer) {
-            const correctAnswer = questions[currentQuestionIndex].correct;
-            
-            if (userAnswer === correctAnswer) {
-                correctSound.currentTime = 0;
-                correctSound.play();
-            } else {
-                wrongSound.currentTime = 0;
-                wrongSound.play();
-            }
+  // img2 배경 변경
+  document.querySelector(".img2").style.background = `
+    linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) 50% 50% / auto 100% no-repeat, 
+    url(${questions[QuestionNumber].background[1]})
+  `;
+  document.querySelector(".img2").style.backgroundPosition = "50% 50%";
+}
 
-            currentQuestionIndex = (currentQuestionIndex + 1) % questions.length;
-            loadQuestion();
-        }
+function nextQuestion(userAnswer) {
+    const correctAnswer = questions[QuestionNumber].correct;
 
-        window.onload = loadQuestion;
+    if (userAnswer === correctAnswer) {
+        correctSound.currentTime = 0;
+        correctSound.play();
+    } else {
+        wrongSound.currentTime = 0;
+        wrongSound.play();
+    }
+
+    QuestionNumber = (QuestionNumber + 1) % questions.length;
+    loadQuestion();
+}
+
+window.onload = loadQuestion;
